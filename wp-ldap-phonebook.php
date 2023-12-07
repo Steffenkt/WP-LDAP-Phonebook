d109cc19f895f4b547c02d1d8ba03c63e51049ce
Steffen Kaster <skaster@kaster-development.de>
2023-12-07 16:07:47 +0100

    Create wp-ldap-phonebook.php

---
+++ wp-ldap-phonebook.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       WordPress LDAP Phonebook
+ * Description:       This plugin serves as a phone book which displays users from an LDAP as a phone book. The phone book is imported from the Active Directory into the WordPress database every 6 hours.
+ * Version:           0.1
+ * Requires at least: 6.0
+ * Requires PHP:      8.1
+ * Author:            Steffen Kaster
+ * Text Domain:       wp-ldap-phonebook
+*/
+
+// Security measure to prevent direct access to the file
+defined('ABSPATH') or die('No script kiddies please!');
+
+?>
